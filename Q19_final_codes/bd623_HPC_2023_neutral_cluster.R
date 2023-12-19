9db107098860d63019d3e43179611df1b58c67f5
Bowen Duan <bowen.duan23@imperial.ac.uk>
2023-12-19 12:47:53 +0000

    modify version

--- Q19_final_codes/bd623_HPC_2023_neutral_cluster.R
+++ Q19_final_codes/bd623_HPC_2023_neutral_cluster.R
@@ -1,19 +1,19 @@
 # CMEE 2022 HPC exercises R code pro forma
 # For neutral model cluster run
 
-rm(list=ls())  # 清空工作区，好的实践
-source("/rds/general/user/bd623/home/run_files/bd623_HPC_2023_main.R")  # 加载所有需要的函数
+rm(list=ls())  # Clear the workspace, a good practice
+source("/rds/general/user/bd623/home/run_files/bd623_HPC_2023_main.R")  # Load all the required functions
 
-# 读取集群作业号（用于集群）
+# Read the cluster job index (for the cluster)
 iter <- as.numeric(Sys.getenv("PBS_ARRAY_INDEX"))
 
-# 本地测试时使用的作业号
+# Job index used for local testing
 #iter <- 2
 
-# 控制随机数种子，确保每次模拟都是唯一的
+# Control the random seed to ensure each simulation is unique
 set.seed(iter)
 
-# 根据作业号选择社区大小
+# Choose community size based on the job index
 if (iter <= 25) {
   size <- 500
 } else if (iter <= 50) {
@@ -24,12 +24,11 @@ if (iter <= 25) {
   size <- 5000
 }
 
-# 设置物种形成率
+# Set the speciation rate
 speciation_rate <- 0.002604  
 
-# 创建存储结果的文件名，确保每个模拟的文件名是唯一的
+# Create a filename to store the results, ensuring each simulation has a unique filename
 result_filename <- paste0("output_", iter, ".rda")
 
-# 调用模拟函数，wall_time设置为11.5小时
-neutral_cluster_run(speciation_rate, size, wall_time = 690, interval_rich = 1, interval_oct = size / 10, burn_in_generations = 8 * size, output_file_name = result_filename)
-
+# Call the simulation function, set wall_time to 11.5 hours
+neutral_cluster_run(speciation_rate, size, wall_time = 1, interval_rich = 1, interval_oct = size / 10, burn_in_generations = 8 * size, output_file_name = result_filename)
