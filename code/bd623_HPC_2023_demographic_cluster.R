716dbb744fd15c84abad38354b92f0b24478f552
Bowen Duan <bowen.duan23@imperial.ac.uk>
2023-12-19 12:35:23 +0000

    modify version

--- code/bd623_HPC_2023_demographic_cluster.R
+++ code/bd623_HPC_2023_demographic_cluster.R
@@ -1,17 +1,17 @@
-# 清理工作区和关闭图形界面
+# Clear the workspace and close graphical interfaces
 rm(list=ls())
 graphics.off()
 
-# 加载函数
-source("/rds/general/user/bd623/home/run_files/bd623_HPC_2023_main.R")  # 加载所有需要的函数
+# Load functions
+source("/rds/general/user/bd623/home/run_files/bd623_HPC_2023_main.R")  # Load all the required functions
 
-# 读取作业编号
+# Read the job index
 iter <- as.numeric(Sys.getenv("PBS_ARRAY_INDEX"))
 
-# 设置随机数种子
+# Set the random seed
 set.seed(iter)
 
-# 选择初始条件
+# Choose initial conditions
 if (iter <= 25) {
   initial_state <- state_initialise_adult(4, 100)
 } else if (iter <= 50) {
@@ -22,10 +22,10 @@ if (iter <= 25) {
   initial_state <- state_initialise_spread(4, 10)
 }
 
-# 创建结果文件名
+# Create the result filename
 result_filename <- paste0("simulation_result_", iter, ".rda")
 
-# 初始化结果列表
+# Initialize a list for results
 results <- vector("list", 150)
 
 clutch_distribution <- c(0.06,0.08,0.13,0.15,0.16,0.18,0.15,0.06,0.03)
@@ -42,11 +42,10 @@ reproduction_matrix <- matrix(c(0.0, 0.0, 0.0, 2.6,
                                 0.0, 0.0, 0.0, 0.0),
                               nrow=4, ncol=4, byrow=TRUE)
 
-
-# 执行模拟并保存结果
+# Run simulations and save results
 for (i in 1:150) {
   results[[i]] <- stochastic_simulation(initial_state, growth_matrix, reproduction_matrix, clutch_distribution, 120)
 }
 
-# 保存模拟结果
+# Save simulation results
 save(results, file=result_filename)
